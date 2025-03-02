43797f11735953cca6561413fddbfdbdb783cf40
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-03-03 04:19:13 +0500

    create topic and entry models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,4 +7,18 @@ class Topic(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.text[0:50]
+        return f"{self.text[0:50]} ..."
+
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        verbose_name_plural = 'entries'
+        ordering = ['-date_added']
+    
+    def __str__(self):
+        return f"Entry (ID: {self.id}) - {self.text[:50]} ..."
+    
